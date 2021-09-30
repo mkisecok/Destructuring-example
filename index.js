e1f9a5d3e5261e0b93bbc04142b7d02e04635d58
Mkisecok <mahmutkisecok@gmail.com>
2021-09-30 10:57:07 +0200

    Add task1

---
+++ index.js
@@ -0,0 +1,8 @@
+// Array Destructuring
+
+const alle=['banana','cucumber','bread','cakes','pizza'];
+const[fruit,vegetable,...food]=alle;
+
+console.log(fruit);
+console.log(vegetable);
+console.log(food);
